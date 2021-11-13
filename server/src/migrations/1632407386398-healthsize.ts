52d0f8745f24b048d42005cfc0a92155052fce21
Diede Exterkate <diede@yoast.com>
2021-11-13 15:16:44 +0100

    Fix failing migration

--- server/src/migrations/1632407386398-healthsize.ts
+++ server/src/migrations/1632407386398-healthsize.ts
@@ -4,13 +4,10 @@ export class healthsize1632407386398 implements MigrationInterface {
     name = 'healthsize1632407386398'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`dnd\`.\`character\` DROP COLUMN \`health\``);
         await queryRunner.query(`ALTER TABLE \`dnd\`.\`character\` ADD \`health\` varchar(50) NOT NULL COMMENT 'The current and total health formatted like current/total (9/10)'`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE \`dnd\`.\`character\` DROP COLUMN \`health\``);
-        await queryRunner.query(`ALTER TABLE \`dnd\`.\`character\` ADD \`health\` varchar(255) NOT NULL COMMENT 'The current and total health formatted like current/total (9/10)'`);
     }
-
 }
